3a51c44c3fe81d02c32401dc1fee37a1b6d6ada9
WaRLoWar <anton.warrior@yandex.ru>
2020-03-03 02:03:46 +0700

    feature (MainForm.cs): Добавлен макет главного окна плагина - Изменено название класса (Form1.cs -> MainForm.cs)

---
+++ CarafeModule/CarafeModuleUI/MainForm.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace CarafeModuleUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.AutoCADMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DisplayModeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.CarafeGroupBox = new System.Windows.Forms.GroupBox();
+            this.HandleLabel = new System.Windows.Forms.Label();
+            this.BottleStopperLabel = new System.Windows.Forms.Label();
+            this.CarafeShapeLabel = new System.Windows.Forms.Label();
+            this.ThroatDiameterLabel = new System.Windows.Forms.Label();
+            this.BaseDiameterLabel = new System.Windows.Forms.Label();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.WidthLength = new System.Windows.Forms.Label();
+            this.LengthLabel = new System.Windows.Forms.Label();
+            this.HandleLengthLabel = new System.Windows.Forms.Label();
+            this.StopperShapeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.HandleComboBox = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.BuildButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.Panel.SuspendLayout();
+            this.CarafeGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AutoCADMenuItem,
+            this.DisplayModeMenuItem,
+            this.HelpMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(599, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // AutoCADMenuItem
+            // 
+            this.AutoCADMenuItem.Name = "AutoCADMenuItem";
+            this.AutoCADMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.AutoCADMenuItem.Text = "AutoCAD";
+            // 
+            // DisplayModeMenuItem
+            // 
+            this.DisplayModeMenuItem.Name = "DisplayModeMenuItem";
+            this.DisplayModeMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.DisplayModeMenuItem.Text = "Display mode";
+            // 
+            // HelpMenuItem
+            // 
+            this.HelpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutMenuItem});
+            this.HelpMenuItem.Name = "HelpMenuItem";
+            this.HelpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.HelpMenuItem.Text = "Help";
+            // 
+            // AboutMenuItem
+            // 
+            this.AboutMenuItem.Name = "AboutMenuItem";
+            this.AboutMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.AboutMenuItem.Text = "About";
+            // 
+            // Panel
+            // 
+            this.Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel.Controls.Add(this.CarafeGroupBox);
+            this.Panel.Location = new System.Drawing.Point(0, 24);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(602, 201);
+            this.Panel.TabIndex = 1;
+            // 
+            // CarafeGroupBox
+            // 
+            this.CarafeGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CarafeGroupBox.Controls.Add(this.BuildButton);
+            this.CarafeGroupBox.Controls.Add(this.comboBox2);
+            this.CarafeGroupBox.Controls.Add(this.textBox7);
+            this.CarafeGroupBox.Controls.Add(this.comboBox1);
+            this.CarafeGroupBox.Controls.Add(this.textBox5);
+            this.CarafeGroupBox.Controls.Add(this.HandleComboBox);
+            this.CarafeGroupBox.Controls.Add(this.textBox4);
+            this.CarafeGroupBox.Controls.Add(this.BaseDiameterLabel);
+            this.CarafeGroupBox.Controls.Add(this.HandleLabel);
+            this.CarafeGroupBox.Controls.Add(this.ThroatDiameterLabel);
+            this.CarafeGroupBox.Controls.Add(this.LengthLabel);
+            this.CarafeGroupBox.Controls.Add(this.textBox6);
+            this.CarafeGroupBox.Controls.Add(this.textBox3);
+            this.CarafeGroupBox.Controls.Add(this.HandleLengthLabel);
+            this.CarafeGroupBox.Controls.Add(this.WidthLength);
+            this.CarafeGroupBox.Controls.Add(this.textBox2);
+            this.CarafeGroupBox.Controls.Add(this.HeightLabel);
+            this.CarafeGroupBox.Controls.Add(this.StopperShapeLabel);
+            this.CarafeGroupBox.Controls.Add(this.textBox1);
+            this.CarafeGroupBox.Controls.Add(this.BottleStopperLabel);
+            this.CarafeGroupBox.Controls.Add(this.CarafeShapeLabel);
+            this.CarafeGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CarafeGroupBox.Location = new System.Drawing.Point(12, 3);
+            this.CarafeGroupBox.Name = "CarafeGroupBox";
+            this.CarafeGroupBox.Size = new System.Drawing.Size(578, 186);
+            this.CarafeGroupBox.TabIndex = 0;
+            this.CarafeGroupBox.TabStop = false;
+            this.CarafeGroupBox.Text = "Carafe";
+            // 
+            // HandleLabel
+            // 
+            this.HandleLabel.AutoSize = true;
+            this.HandleLabel.Location = new System.Drawing.Point(245, 23);
+            this.HandleLabel.Name = "HandleLabel";
+            this.HandleLabel.Size = new System.Drawing.Size(47, 15);
+            this.HandleLabel.TabIndex = 7;
+            this.HandleLabel.Text = "Handle";
+            // 
+            // BottleStopperLabel
+            // 
+            this.BottleStopperLabel.AutoSize = true;
+            this.BottleStopperLabel.Location = new System.Drawing.Point(14, 78);
+            this.BottleStopperLabel.Name = "BottleStopperLabel";
+            this.BottleStopperLabel.Size = new System.Drawing.Size(82, 15);
+            this.BottleStopperLabel.TabIndex = 6;
+            this.BottleStopperLabel.Text = "Bottle stopper";
+            // 
+            // CarafeShapeLabel
+            // 
+            this.CarafeShapeLabel.AutoSize = true;
+            this.CarafeShapeLabel.Location = new System.Drawing.Point(16, 107);
+            this.CarafeShapeLabel.Name = "CarafeShapeLabel";
+            this.CarafeShapeLabel.Size = new System.Drawing.Size(80, 15);
+            this.CarafeShapeLabel.TabIndex = 5;
+            this.CarafeShapeLabel.Text = "Carafe shape";
+            // 
+            // ThroatDiameterLabel
+            // 
+            this.ThroatDiameterLabel.AutoSize = true;
+            this.ThroatDiameterLabel.Location = new System.Drawing.Point(2, 50);
+            this.ThroatDiameterLabel.Name = "ThroatDiameterLabel";
+            this.ThroatDiameterLabel.Size = new System.Drawing.Size(94, 15);
+            this.ThroatDiameterLabel.TabIndex = 4;
+            this.ThroatDiameterLabel.Text = "Throat diameter";
+            // 
+            // BaseDiameterLabel
+            // 
+            this.BaseDiameterLabel.AutoSize = true;
+            this.BaseDiameterLabel.Location = new System.Drawing.Point(9, 23);
+            this.BaseDiameterLabel.Name = "BaseDiameterLabel";
+            this.BaseDiameterLabel.Size = new System.Drawing.Size(87, 15);
+            this.BaseDiameterLabel.TabIndex = 3;
+            this.BaseDiameterLabel.Text = "Base diameter";
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Location = new System.Drawing.Point(412, 78);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(43, 15);
+            this.HeightLabel.TabIndex = 2;
+            this.HeightLabel.Text = "Height";
+            // 
+            // WidthLength
+            // 
+            this.WidthLength.AutoSize = true;
+            this.WidthLength.Location = new System.Drawing.Point(417, 51);
+            this.WidthLength.Name = "WidthLength";
+            this.WidthLength.Size = new System.Drawing.Size(38, 15);
+            this.WidthLength.TabIndex = 1;
+            this.WidthLength.Text = "Width";
+            // 
+            // LengthLabel
+            // 
+            this.LengthLabel.AutoSize = true;
+            this.LengthLabel.Location = new System.Drawing.Point(410, 23);
+            this.LengthLabel.Name = "LengthLabel";
+            this.LengthLabel.Size = new System.Drawing.Size(45, 15);
+            this.LengthLabel.TabIndex = 0;
+            this.LengthLabel.Text = "Length";
+            // 
+            // HandleLengthLabel
+            // 
+            this.HandleLengthLabel.AutoSize = true;
+            this.HandleLengthLabel.Location = new System.Drawing.Point(210, 52);
+            this.HandleLengthLabel.Name = "HandleLengthLabel";
+            this.HandleLengthLabel.Size = new System.Drawing.Size(84, 15);
+            this.HandleLengthLabel.TabIndex = 8;
+            this.HandleLengthLabel.Text = "Handle length";
+            // 
+            // StopperShapeLabel
+            // 
+            this.StopperShapeLabel.AutoSize = true;
+            this.StopperShapeLabel.Location = new System.Drawing.Point(210, 78);
+            this.StopperShapeLabel.Name = "StopperShapeLabel";
+            this.StopperShapeLabel.Size = new System.Drawing.Size(87, 15);
+            this.StopperShapeLabel.TabIndex = 9;
+            this.StopperShapeLabel.Text = "Stopper shape";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(461, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(461, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(461, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(102, 20);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(102, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(298, 49);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 15;
+            // 
+            // HandleComboBox
+            // 
+            this.HandleComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.HandleComboBox.FormattingEnabled = true;
+            this.HandleComboBox.Items.AddRange(new object[] {
+            "Enable",
+            "Disable"});
+            this.HandleComboBox.Location = new System.Drawing.Point(298, 20);
+            this.HandleComboBox.Name = "HandleComboBox";
+            this.HandleComboBox.Size = new System.Drawing.Size(100, 23);
+            this.HandleComboBox.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Enable",
+            "Disable"});
+            this.comboBox1.Location = new System.Drawing.Point(102, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 23);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(298, 78);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 18;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Square",
+            "Round",
+            "Default"});
+            this.comboBox2.Location = new System.Drawing.Point(102, 104);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 23);
+            this.comboBox2.TabIndex = 19;
+            // 
+            // BuildButton
+            // 
+            this.BuildButton.Location = new System.Drawing.Point(234, 142);
+            this.BuildButton.Name = "BuildButton";
+            this.BuildButton.Size = new System.Drawing.Size(102, 26);
+            this.BuildButton.TabIndex = 20;
+            this.BuildButton.Text = "Build";
+            this.BuildButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 225);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(672, 438);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoCAD:  Carafe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.Panel.ResumeLayout(false);
+            this.CarafeGroupBox.ResumeLayout(false);
+            this.CarafeGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem AutoCADMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DisplayModeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutMenuItem;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.GroupBox CarafeGroupBox;
+        private System.Windows.Forms.Label HandleLabel;
+        private System.Windows.Forms.Label BottleStopperLabel;
+        private System.Windows.Forms.Label CarafeShapeLabel;
+        private System.Windows.Forms.Label ThroatDiameterLabel;
+        private System.Windows.Forms.Label BaseDiameterLabel;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.Label WidthLength;
+        private System.Windows.Forms.Label LengthLabel;
+        private System.Windows.Forms.Label StopperShapeLabel;
+        private System.Windows.Forms.Label HandleLengthLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox HandleComboBox;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button BuildButton;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
